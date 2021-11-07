ca6c941e9acfbf82f466621fa0c81ab9365f9726
Darkknight131714 <darkknight131714@gmail.com>
2021-11-07 22:05:34 +0530

    YO

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,19 @@ import 'package:flutter/material.dart';
 import 'loginpage.dart';
 import 'homepage.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const Loginpage(),
     );
   }
 }
-
